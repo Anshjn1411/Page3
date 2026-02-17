c9162daf0b8a2730b3f508a7c23f7f39b64a3cf3
Anshjn1411 <anshjn1411@gmail.com>
2026-02-17 16:59:08 +0530

    Update the Sdk Part Android

--- run_ios.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the framework
-./gradlew :composeApp:embedAndSignAppleFrameworkForXcode
-
-# Open Xcode
-open iosApp/iosApp.xcodeproj
-
-echo "✅ Framework built! Now click ▶️ in Xcode to run"
